5cc21b686ab47bd56d2d0dbec33e049f0e03568f
Talha Aydin <talha.aydin.87@icloud.com>
2017-10-12 15:11:23 +0200

    Add income part

---
+++ src/app/income/redux.income.js
@@ -0,0 +1,40 @@
+var INCOME = {
+  ADD: 'INCOME.ADD', // Adds single income
+  REMOVE: 'INCOME.REMOVE', // Removes single income
+  CHANGE: 'INCOME.CHANGE', // Replace single income
+  SELECT: 'INCOME.SELECT', // Sets new selected array
+  FORM: {
+    NEW: 'INCOME.FORM.NEW',
+    EDIT: 'INCOME.FORM.EDIT',
+    RESET: 'INCOME.FORM.RESET'
+  }
+}
+
+var incomeReducer = function (state = {
+  node: 'income',
+  data: [],
+  selected: [],
+  form: { opened: false, editKey: null }
+}, action) {
+  const { ADD, SELECT, REMOVE, CHANGE, FORM } = INCOME;
+  switch (action.type) {
+    case ADD:
+      return Object.assign({}, state, { data: [...state.data, action.payload] });
+    case SELECT:
+      return Object.assign({}, state, { selected: [...action.payload] });
+    case REMOVE:
+      return Object.assign({}, state, { data: state.data.filter(e => e.key !== action.payload) });
+    case CHANGE: {
+      const newData = state.data.map(e => e.key === action.payload.key ? action.payload : e);
+      return Object.assign({}, state, { data: newData });
+    }
+    case FORM.NEW:
+      return Object.assign({}, state, { form: { opened: true, editKey: null } });
+    case FORM.EDIT:
+      return Object.assign({}, state, { form: { opened: true, editKey: action.payload } });
+    case FORM.RESET:
+      return Object.assign({}, state, { form: { opened: false, editKey: null } });
+    default:
+      return state;
+  }
+}
--- src/app/redux.store.js
+++ src/app/redux.store.js
@@ -1,4 +1,5 @@
 var reduxStore = Redux.createStore(Redux.combineReducers({
   transactions: transactionsReducer,
-  expenses: expensesReducer
+  expenses: expensesReducer,
+  income: incomeReducer
 }), /* preloaded skipped, */ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
